67dc3c22bd137a951a96b60f1575f2c12466a6ea
Jackson Iyamu <jacksonuwa1@gmail.com>
2024-08-16 16:04:40 +0100

    context feature

--- src/container/App.js
+++ src/container/App.js
@@ -7,6 +7,8 @@ import {
 
 import Layout from "../shared/Layout/layout";
 
+import { FeedbackProvider } from "../context/feedbackContext";
+
 import Home from "../pages/home/home";
 import Register from "../pages/register/register";
 import Login from "../pages/log/login";
@@ -17,31 +19,33 @@ import Review from "../pages/reviews/review";
 
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/forgot-password">
-            <PasswordPage />
-          </Route>
-          <Route path="/dashboard">
-            <DashBoard />
-          </Route>
-          <Route path="/feedback-reviews">
-            <Review />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </Layout>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/forgot-password">
+              <PasswordPage />
+            </Route>
+            <Route path="/dashboard">
+              <DashBoard />
+            </Route>
+            <Route path="/feedback-reviews">
+              <Review />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Layout>
+      </Router>
+    </FeedbackProvider>
   );
 };
 
---
+++ src/context/feedbackContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+import FeedBack from "../components/review/reviews/data/feedback";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = (props) => {
+  const [feedback, setFeedback] = useState(FeedBack);
+  return (
+    <FeedbackContext.Provider value={{ feedback }}>
+      {console.log("From the context wrapper ")}
+      {props.children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
