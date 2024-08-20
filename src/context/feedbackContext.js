c49f77950ab66f00f0fe5c1a604dbb4d3307a903
Jackson Iyamu <jacksonuwa1@gmail.com>
2024-08-20 15:06:15 +0100

    new feature

--- src/context/feedbackContext.js
+++ src/context/feedbackContext.js
@@ -4,7 +4,7 @@ import FeedBack from "../components/review/reviews/data/feedback";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = (props) => {
-  const [feedback, setFeedback] = useState(FeedBack);
+  const [feedback] = useState(FeedBack);
   return (
     <FeedbackContext.Provider value={{ feedback }}>
       {console.log("From the context wrapper ")}
